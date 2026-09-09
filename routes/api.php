abd386b3a71895cde4dc6899958aa7b8479ca2ae
Iitizaz Ur Rehman <72123604+AitizazRehman@users.noreply.github.com>
2026-09-09 16:03:29 +0500

    Connect Customer API routes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\BranchController;
 use App\Http\Controllers\API\ProjectController;
@@ -11,201 +10,33 @@ use App\Http\Controllers\API\PropertyImageController;
 use App\Http\Controllers\API\PropertyDocumentController;
 use App\Http\Controllers\API\PropertyFeatureController;
 use App\Http\Controllers\API\PropertyStatusController;
-
-
-/*
-|--------------------------------------------------------------------------
-| Authentication
-|--------------------------------------------------------------------------
-*/
+use App\Http\Controllers\API\CustomerController;
 
 Route::prefix('auth')->group(function () {
-
-    Route::post('/login', [
-        AuthController::class,
-        'login'
-    ]);
+    Route::post('/login', [AuthController::class, 'login']);
 });
 
-
-/*
-|--------------------------------------------------------------------------
-| Protected API Routes
-|--------------------------------------------------------------------------
-*/
-
 Route::middleware('auth:sanctum')->group(function () {
-
-
-    /*
-    |--------------------------------------------------------------------------
-    | Authentication
-    |--------------------------------------------------------------------------
-    */
-
     Route::prefix('auth')->group(function () {
-
-        Route::get('/me', [
-            AuthController::class,
-            'me'
-        ]);
-
-        Route::post('/logout', [
-            AuthController::class,
-            'logout'
-        ]);
+        Route::get('/me', [AuthController::class, 'me']);
+        Route::post('/logout', [AuthController::class, 'logout']);
     });
 
+    Route::apiResource('branches', BranchController::class)->only(['index', 'show']);
+    Route::apiResource('projects', ProjectController::class);
+    Route::apiResource('project-blocks', ProjectBlockController::class);
 
-    /*
-    |--------------------------------------------------------------------------
-    | Branches / Offices
-    |--------------------------------------------------------------------------
-    */
+    Route::get('properties/inventory', [PropertyController::class, 'inventory']);
+    Route::apiResource('properties', PropertyController::class);
+    Route::put('properties/{property}/status', [PropertyStatusController::class, 'update']);
+    Route::get('properties/{property}/status-history', [PropertyStatusController::class, 'history']);
+    Route::post('properties/{property}/images', [PropertyImageController::class, 'store']);
+    Route::delete('property-images/{image}', [PropertyImageController::class, 'destroy']);
+    Route::put('property-images/{image}/primary', [PropertyImageController::class, 'primary']);
+    Route::post('properties/{property}/documents', [PropertyDocumentController::class, 'store']);
+    Route::delete('property-documents/{document}', [PropertyDocumentController::class, 'destroy']);
+    Route::post('properties/{property}/features', [PropertyFeatureController::class, 'store']);
+    Route::delete('property-features/{feature}', [PropertyFeatureController::class, 'destroy']);
 
-    Route::apiResource('branches', BranchController::class)
-        ->only([
-            'index',
-            'show'
-        ]);
-
-
-    /*
-    |--------------------------------------------------------------------------
-    | Projects
-    |--------------------------------------------------------------------------
-    */
-
-    Route::apiResource(
-        'projects',
-        ProjectController::class
-    );
-
-
-    /*
-    |--------------------------------------------------------------------------
-    | Project Blocks
-    |--------------------------------------------------------------------------
-    */
-
-    Route::apiResource(
-        'project-blocks',
-        ProjectBlockController::class
-    );
-
-
-    /*
-    |--------------------------------------------------------------------------
-    | Properties
-    |--------------------------------------------------------------------------
-    */
-
-    Route::get(
-        'properties/inventory',
-        [
-            PropertyController::class,
-            'inventory'
-        ]
-    );
-
-    Route::apiResource(
-        'properties',
-        PropertyController::class
-    );
-
-
-    /*
-    |--------------------------------------------------------------------------
-    | Property Status
-    |--------------------------------------------------------------------------
-    */
-
-    Route::put(
-        'properties/{property}/status',
-        [
-            PropertyStatusController::class,
-            'update'
-        ]
-    );
-
-    Route::get(
-        'properties/{property}/status-history',
-        [
-            PropertyStatusController::class,
-            'history'
-        ]
-    );
-
-
-    /*
-    |--------------------------------------------------------------------------
-    | Property Images
-    |--------------------------------------------------------------------------
-    */
-
-    Route::post(
-        'properties/{property}/images',
-        [
-            PropertyImageController::class,
-            'store'
-        ]
-    );
-
-    Route::delete(
-        'property-images/{image}',
-        [
-            PropertyImageController::class,
-            'destroy'
-        ]
-    );
-    Route::put(
-        'property-images/{image}/primary',
-        [PropertyImageController::class, 'primary']
-    );
-
-
-    /*
-    |--------------------------------------------------------------------------
-    | Property Documents
-    |--------------------------------------------------------------------------
-    */
-
-    Route::post(
-        'properties/{property}/documents',
-        [
-            PropertyDocumentController::class,
-            'store'
-        ]
-    );
-
-    Route::delete(
-        'property-documents/{document}',
-        [
-            PropertyDocumentController::class,
-            'destroy'
-        ]
-    );
-
-
-    /*
-    |--------------------------------------------------------------------------
-    | Property Features
-    |--------------------------------------------------------------------------
-    */
-
-    Route::post(
-        'properties/{property}/features',
-        [
-            PropertyFeatureController::class,
-            'store'
-        ]
-    );
-
-    Route::delete(
-        'property-features/{feature}',
-        [
-            PropertyFeatureController::class,
-            'destroy'
-        ]
-    );
+    Route::apiResource('customers', CustomerController::class);
 });
