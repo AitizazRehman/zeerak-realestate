c6e8c50b21536cdfdd6164756b8de6c2fb8138e8
Iitizaz Ur Rehman <72123604+AitizazRehman@users.noreply.github.com>
2026-09-22 15:21:57 +0500

    Support user activation status

--- app/Models/User.php
+++ app/Models/User.php
@@ -16,6 +16,7 @@ class User extends Authenticatable
         'email',
         'password',
         'branch_id',
+        'is_active',
     ];
 
     protected $hidden = [
@@ -25,6 +26,7 @@ class User extends Authenticatable
 
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'is_active' => 'boolean',
     ];
     
     public function branch()
