ecee271d04401ed9446ba7b6dadb420df30cebc6
Iitizaz Ur Rehman <72123604+AitizazRehman@users.noreply.github.com>
2026-09-23 15:01:42 +0500

    Serve SPA root through cacheable controller

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SpaController;
 
+Route::get('/', [SpaController::class, 'index']);
+
 Route::get('/{any}', [SpaController::class, 'index'])
     ->where('any', '^(?!api).*$');
